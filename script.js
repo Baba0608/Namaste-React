0ea19514fe76a1cc1292bb194b006cdbe5103697
Baba0608 <babafakruddinnitturu@gmail.com>
2023-10-14 10:51:15 +0530

    Inception

---
+++ script.js
@@ -0,0 +1,12 @@
+const heading = ReactDOM.createRoot(document.getElementById("heading"));
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ])
+);
+
+heading.render(parent);
